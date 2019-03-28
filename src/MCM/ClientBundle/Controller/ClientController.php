4d91330a9e7dcf4852a37726b9787cc0bb618673
María <mariiamlg.26@gmail.com>
2019-03-28 23:44:18 +0100

    Corregido proceso modificación cliente

--- src/MCM/ClientBundle/Controller/ClientController.php
+++ src/MCM/ClientBundle/Controller/ClientController.php
@@ -15,7 +15,6 @@ class ClientController extends Controller
 {
     /* Pinta el formulario */
     public function formAction(Request $request) {
-
         /*Si recibe un id deberá rellenar el formulario con toda la información de ese cliente*/
         if($request->query->get('id')) {
             $id = $request->query->get('id');
@@ -33,34 +32,42 @@ class ClientController extends Controller
     /* Ejecuta la query correspondiente (insert o update) */
     public function queryAction (Request $request) {
         $modal = false;
+        $id = '';
+
         $clientEmpty = new Client();
+        $clientId = $request->request->get('mcm_clientbundle_client')['id'];
         $form = $this->createForm(new ClientType(), $clientEmpty);
-        $form->handleRequest($request);
+        /*Si recibe un Id tiene que actualizar al cliente*/
+        if(!empty($clientId)) {
+            $em = $this->getDoctrine()->getManager();
+            $client = $em->getRepository(Client::class)->find($clientId);
 
-        $client = $form->getData();
-
-        /* Si en el formulario no está vacío el campo Id corresponderá un update */
-        if(isset($request->request->get('mcm_clientbundle_client')['id']) && !empty($request->request->get('mcm_clientbundle_client')['id'])) {
-            $this->update($client);
+            $this->update($request, $client);
         } else { /* Será el insert de un cliente*/
+            $client = $form->getData();
+            $form->handleRequest($request);
+
             if ($form->isSubmitted() && $form->isValid()) {
-                $exists = $this->exists($client);
+                $responseExists = $this->exists($client);
+                $exists = $responseExists['exists'];
 
                 if(!$exists) {
                     $this->create($request, $client);
                 } else {
                     $modal = true;
+                    $id = $responseExists['id'];
                 }          
             }
         }
 
         return $this->render('MCMClientBundle:Client:form.html.twig', array(
-            'form' => $form->createView(), 'modal'=> $modal
+            'form' => $form->createView(), 'modal' => $modal, 'id' => $id
         ));
     }
 
     /* Crea un formulario con los datos de un cliente */
     private function createFormEdit($idClient) {
+
         $em = $this->getDoctrine()->getManager();
         $client = $em->getRepository(Client::class)->findOneBy(array('id' => $idClient));
 
@@ -84,12 +91,29 @@ class ClientController extends Controller
     }
 
     /* Actualiza los valores de un cliente */
-    private function update($form) {
-        $clientId = $request->request->get('id');;
-        $client = $em->getRepository(Client::class)->find($clientId);
+    private function update(Request $request, $client) {
+        $client->setFullName($request->request->get('mcm_clientbundle_client')['fullName']);
+        $client->setEmail($request->request->get('mcm_clientbundle_client')['email']);
+        $client->setIdCard($request->request->get('mcm_clientbundle_client')['idCard']);
+        if(isset($request->request->get('mcm_clientbundle_client')['subscribed'])) {
+            $client->setSubscribed(true);
+        } else {
+            $client->setSubscribed(false);
+        }
+        $client->setTypeCachtment($request->request->get('mcm_clientbundle_client')['typeCachtment']);
+        $client->setAddress($request->request->get('mcm_clientbundle_client')['address']);
+        if(empty($request->request->get('mcm_clientbundle_client')['zipCode'])){
+            $zipCode = NULL;
+        } else {
+            $zipCode = $request->request->get('mcm_clientbundle_client')['zipCode'];
+        }
+        $client->setZipCode($zipCode);
+        $client->setState($request->request->get('mcm_clientbundle_client')['state']);
+        $client->setCity($request->request->get('mcm_clientbundle_client')['city']);
+        $client->setCountry($request->request->get('mcm_clientbundle_client')['country']);
+        $client->setComments($request->request->get('mcm_clientbundle_client')['comments']);
 
         $em = $this->getDoctrine()->getManager();
-        $em->persist($client);
         $em->flush();
 
         $request->getSession()
@@ -100,6 +124,7 @@ class ClientController extends Controller
     /* Comprueba si existe un cliente con ese email o documento de identidad*/
     private function exists($client) {
         $exists = false;
+        $id = '';
 
         $email = $client->getEmail();
         $idCard = $client->getIdCard();
@@ -110,8 +135,15 @@ class ClientController extends Controller
 
         if ($client1 || $client2) {
             $exists = true;
+            if ($client1) {
+                $id = $client1->getId();
+            } else {
+                $id = $client2->getId();
+            }
         }
 
-        return $exists;
+        $response = array('exists' => $exists, 'id' => $id);
+
+        return $response;
     }
 }
